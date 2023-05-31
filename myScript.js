22cb5882f5be4445837de169b1763fe7e683f765
juanaarrietaa <jarrieta@itba.edu.ar>
2023-05-30 21:01:15 -0300

    clase 12.3

--- myScript.js
+++ myScript.js
@@ -4,10 +4,10 @@ const descu1=20
 const descu2=15
 
 let preciofinal1= precio1*(descu1/100)
-console.log(preciofinal1)
+console.log(precio1-preciofinal1)
 let preciofinal2= precio2*(descu2/100)
-console.log(preciofinal2)
+console.log(precio2-preciofinal2)
 let preciofinal3= precio1*(descu2/100)
-console.log(preciofinal3)
+console.log(precio1-preciofinal3)
 let preciofinal4= precio2*(descu1/100)
-console.log(preciofinal4)
+console.log(precio2-preciofinal4)
