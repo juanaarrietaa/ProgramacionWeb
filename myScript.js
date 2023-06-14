69ad827f42923b83dba2a7391b7000e989d19d7c
juanaarrietaa <jarrieta@itba.edu.ar>
2023-06-13 23:27:26 -0300

    ultima clase

--- myScript.js
+++ myScript.js
@@ -11,3 +11,23 @@ let preciofinal3= precio1*(descu2/100)
 console.log(precio1-preciofinal3)
 let preciofinal4= precio2*(descu1/100)
 console.log(precio2-preciofinal4)
+
+
+function guardar(){
+
+    let valor=5000
+    let valor2=7200
+    valor=document.getElementById("value").value  
+    localStorage.setItem("valor_curso", valor)
+    localStorage.setItem("valor_curso", valor2)
+    localStorage.getItem("valor_curso", valor)
+    console.log(valor)
+    
+    
+    }
+    
+    
+    document.getElementById("guardar").addEventListener("click",guardar)
+    
+    localStorage.clear()
+    localStorage.removeItem("valor_curso")
