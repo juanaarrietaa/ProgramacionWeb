375576f4742a1e91e4a4d66dc76d055e173a72f9
juanaarrietaa <jarrieta@itba.edu.ar>
2023-06-18 01:33:44 -0300

    cambios en la paguina de cursos

--- myScript.js
+++ myScript.js
@@ -1,33 +1,56 @@
-const precio1= 28000
-const precio2=300000
+const comprarcontenido= document.getElementById("comprarcontenido");
+
+
+
+
+const precio1= 25000
+const precio2= 15000
+const precio3= 18000
+const precio4= 35000
+const precio5= 22000
+const precio6=15000
 const descu1=20
-const descu2=15
 
 let preciofinal1= precio1*(descu1/100)
 console.log(precio1-preciofinal1)
-let preciofinal2= precio2*(descu2/100)
+let preciofinal2= precio2*(descu1/100)
 console.log(precio2-preciofinal2)
-let preciofinal3= precio1*(descu2/100)
-console.log(precio1-preciofinal3)
-let preciofinal4= precio2*(descu1/100)
-console.log(precio2-preciofinal4)
+let preciofinal3= precio3*(descu1/100)
+console.log(precio3-preciofinal3)
+let preciofinal4= precio4*(descu1/100)
+console.log(precio4-preciofinal4)
+let preciofinal5= precio5*(descu1/100)
+console.log(precio5-preciofinal5)
+let preciofinal6= precio6*(descu1/100)
+console.log(precio6-preciofinal6)
+
+let carrito=[]
 
 
-function guardar(){
 
-    let valor=5000
-    let valor2=7200
-    valor=document.getElementById("value").value  
-    localStorage.setItem("valor_curso", valor)
-    localStorage.setItem("valor_curso", valor2)
-    localStorage.getItem("valor_curso", valor)
-    console.log(valor)
+
+
+
+
+
+
+
+
+// function guardar(){
+
+//     let valor=5000
+//     let valor2=7200
+//     valor=document.getElementById("value").value  
+//     localStorage.setItem("valor_curso", valor)
+//     localStorage.setItem("valor_curso", valor2)
+//     localStorage.getItem("valor_curso", valor)
+//     console.log(valor)
     
     
-    }
+//     }
     
     
-    document.getElementById("guardar").addEventListener("click",guardar)
+//     document.getElementById("guardar").addEventListener("click",guardar)
     
-    localStorage.clear()
-    localStorage.removeItem("valor_curso")
+//     localStorage.clear()
+//     localStorage.removeItem("valor_curso")
