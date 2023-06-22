b459add5fee9de37cbce4473b18e29f098615f6d
juanaarrietaa <jarrieta@itba.edu.ar>
2023-06-22 14:32:38 -0300

    ultimos cambios casi listo

--- myScript.js
+++ myScript.js
@@ -1,88 +1,40 @@
-// Obtener referencia a los botones de compra
-var botonesComprar = document.getElementsByClassName('btn-compra');
+let total = 0;
+let cantidad = 0;
 
-// Agregar evento de clic a los botones de compra
-for (var i = 0; i < botonesComprar.length; i++) {
-  botonesComprar[i].addEventListener('click', agregarAlCarrito);
+function comprar(valorproducto) {
+  total = total + valorproducto;
+  document.getElementById("total-a-pagar").innerHTML = "$" + total;
+  cantidad += 1;
+
+  document.getElementById("cantidad").innerHTML = cantidad;
+  document.getElementById("no-productos").innerHTML = " ";
 }
 
-// Carrito de compras
-var carrito = [];
+function descomprar(valorproducto) {
+  if (cantidad > 0) {
+    total = total - valorproducto;
+    document.getElementById("total-a-pagar").innerHTML = "$" + total;
+    cantidad -= 1;
+    document.getElementById("cantidad").innerHTML = cantidad;
+    document.getElementById("no-productos").innerHTML = " ";
+  }
+}
 
+function vaciar() {
+  total = 0;
+  document.getElementById("total-a-pagar").innerHTML = " $" + total;
+  document.getElementById("no-productos").innerHTML =
+    "No hay productos en el carrito.";
+  cantidad = 0;
+  document.getElementById("cantidad").innerHTML = cantidad;
+}
 
-function agregarAlCarrito() {
-  var producto = {
-    nombre: this.parentNode.querySelector('p').textContent,
-    precio: parseFloat(this.parentNode.querySelector('.precio').textContent.slice(1)),
-    cantidad: 1
-  };
-
-  // Verificar si el producto ya está en el carrito
-  var productoExistente = carrito.find(function(item) {
-    return item.nombre === producto.nombre;
-  });
-
-  if (productoExistente) {
-    
-    productoExistente.cantidad++;
+function validarCaptcha() {
+  var response = grecaptcha.getResponse();
+  if (response.length === 0) {
+    console.log("Por favor, completa el captcha.");
+    return false;
   } else {
-    
-    carrito.push(producto);
-  }
-
-  actualizarCarrito();
-}
-
-// Función para actualizar el carrito
-function actualizarCarrito() {
-  var carritoBody = document.getElementById('carrito-body');
-  var totalCarrito = document.getElementById('total-precio');
-  var vaciarCarritoBtn = document.getElementById('vaciar-carrito');
-
-  carritoBody.innerHTML = '';
-  totalCarrito.textContent = '$0';
-
-  for (var i = 0; i < carrito.length; i++) {
-    var producto = carrito[i];
-    var precioTotal = producto.precio * producto.cantidad;
-
-    var fila = document.createElement('tr');
-    fila.innerHTML = `
-      <td>${producto.nombre}</td>
-      <td>$${producto.precio}</td>
-      <td>${producto.cantidad}</td>
-      <td>$${precioTotal}</td>
-      <td><button class="btn-eliminar" data-indice="${i}">Eliminar</button></td>
-    `;
-
-    carritoBody.appendChild(fila);
-  }
-
-  var total = carrito.reduce(function(acc, producto) {
-    return acc + producto.precio * producto.cantidad;
-  }, 0);
-
-  totalCarrito.textContent = '$' + total.toFixed(2);
-
-  vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
-  var botonesEliminar = document.getElementsByClassName('btn-eliminar');
-  for (var j = 0; j < botonesEliminar.length; j++) {
-    botonesEliminar[j].addEventListener('click', eliminarProducto);
+    return true;
   }
 }
-
-
-function vaciarCarrito() {
-  carrito = [];
-  actualizarCarrito();
-}
-
-
-function eliminarProducto() {
-  var indice = parseInt(this.dataset.indice);
-  carrito.splice(indice, 1);
-  actualizarCarrito();
-}
-
-// Inicializar el carrito
-actualizarCarrito();
